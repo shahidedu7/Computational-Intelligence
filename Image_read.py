86c12f7aaffdc819b749d9eb365c6f59ea49f370
Shahid Akhtar Khan <63913889+shahidakhtarkhan@users.noreply.github.com>
2020-10-14 15:58:50 +0530

    Update Image_read.py

--- Image_read.py
+++ Image_read.py
@@ -1,4 +1,5 @@
+# this is for delete
 import matplotlib.pyplot as plt
-image_path = r'C:\Users\Public\Pictures\Sample Pictures\Desert.jpg'
+image_path = r'C:\Users\Public\Pictures\Sample Pictures\Desert.jpg'   # put your own image path
 image = plt.imread(image_path)
 plt.imshow(image)
