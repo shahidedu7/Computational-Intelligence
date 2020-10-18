70799f0d252bf1b20bb73a576bad5cb093d869ba
Shahid Akhtar Khan <63913889+shahidakhtarkhan@users.noreply.github.com>
2020-10-18 10:07:15 +0530

    Update Image_read.py

--- Image_read.py
+++ Image_read.py
@@ -3,3 +3,12 @@ import matplotlib.pyplot as plt
 image_path = r'C:\Users\Public\Pictures\Sample Pictures\Desert.jpg'   # put your own image path
 image = plt.imread(image_path)
 plt.imshow(image)
+
+
+import cv2
+image_path = r'C:\Users\Public\Pictures\Sample Pictures\Desert.jpg'
+image = cv2.imread(image_path)
+plt.imshow(image)
+cv2.imshow('image',image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
